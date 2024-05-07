dd8c79755d7a0eeb20007903d3c61e2bad5afe40
JouTiago <162067688+JouTiago@users.noreply.github.com>
2024-05-07 01:57:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,74 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
+
+app.use((req, res, next) => {
+  console.log(`Recebida solicitação: ${req.method} ${req.url}`);
+  next();
+});
 
 const port = process.env.PORT || 3000;
 
-const carrosData = JSON.parse(fs.readFileSync('carrosbrasil.json', 'utf-8'));
+let carrosData;
+try {
+  carrosData = JSON.parse(fs.readFileSync("carrosbrasil.json", "utf-8"));
+} catch (error) {}
 
-app.get('/consultarCarro', (req, res) => {
-    const input = req.query.input.toLowerCase().replace(/[^a-zA-Z0-9\s]/gi, ''); 
-    const palavras = input.split(/\s+/); 
+// console.log("Modelos armazenados no mapa:", carrosData);
 
-    let marcaEncontrada = false;
-    let modeloEncontrado = false;
+app.post("/consultarCarro", (req, res) => {
+  console.log("Recebida solicitação para consultar carro.");
 
-    
-    for (let palavra of palavras) {
-        const carro = carrosData.find(carro => carro.marca.toLowerCase() === palavra);
-        if (carro) {
-            marcaEncontrada = true;
-            break;
-        }
+  console.log(req.query);
+  console.log(req.body);
+
+  const input = (req.query.input || "")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9\s]/gi, "");
+  const palavras = input.split(/\s+/);
+  console.log(req.body);
+  console.log("Palavras a serem pesquisadas:", palavras);
+
+  let marcaEncontrada = false;
+  let modeloEncontrado = false;
+
+  for (const item of carrosData) {
+    for (const pal of palavras) {
+      if (pal == item.marca) {
+        marcaEncontrada = true;
+      }
+      if (pal == item.modelo) {
+        modeloEncontrado = true;
+      }
     }
+  }
 
-    
-    if (marcaEncontrada) {
-        for (let palavra of palavras) {
-            const carro = carrosData.find(carro => carro.modelo.toLowerCase() === palavra);
-            if (carro) {
-                modeloEncontrado = true;
-                break;
-            }
-        }
-    }
+  // for (let palavra of palavras) {
+  //   const modelos = marcasMap.get(palavra);
+  //   console.log("Modelos encontrados para", palavra, ":", modelos);
+  //   if (modelos) {
+  //     marcaEncontrada = true;
+  //     if (modelos.has(palavra)) {
+  //       modeloEncontrado = true;
+  //       break;
+  //     }
+  //   }
+  // }
 
-    
-    if (marcaEncontrada && modeloEncontrado) {
-        res.json({ mensagem: 'Marca e modelo do carro validados.' });
-    } else {
-        res.status(404).json({ error: 'Marca ou modelo do carro não encontrados.' });
-    }
+  console.log("Marca encontrada:", marcaEncontrada);
+  console.log("Modelo encontrado:", modeloEncontrado);
+
+  if (marcaEncontrada && modeloEncontrado) {
+    const resposta = { mensagem: "Marca e modelo do carro validados." };
+    console.log("Enviando resposta:", resposta);
+    res.json(resposta);
+  } else {
+    const erro = { error: "Marca ou modelo do carro não encontrados." };
+    console.log("Enviando resposta de erro:", erro);
+    res.status(404).json(erro);
+  }
 });
 
-
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://0.0.0.0:${port}`);
 });
